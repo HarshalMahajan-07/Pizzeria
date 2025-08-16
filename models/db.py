9bc0e405e15eb8ce7e78159d5f0b7e35bc663734
Harshal Mahajan <harshalm577@gmail.com>
2025-08-16 13:54:01 +0530

    Update db.py

--- models/db.py
+++ models/db.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
 def get_db():
-    client = MongoClient("mongodb+srv://harshop732:8Iki4hwm3WQ8P7m5@cluster0.jwv48iz.mongodb.net/")
+    client = MongoClient("your mongodb connection string")
     db = client["pizza_app"]
     return db
